f483011bc68f6ef9244994d00e8960104a872444
Pranjal Singh Karakoti <pranjalskarakoti33@gmail.com>
2024-11-11 19:30:56 +0530

    finale

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function play(playerChoice){
     result= 'Draw';
     } 
  else if(playerChoice==='rock'){
-        if(computerChoice==='paper'){
+        if(computerChoice==='hand'){
             result='You lose.';
         } 
         else if (computerChoice==='scissors') {
@@ -16,12 +16,12 @@ function play(playerChoice){
         if (computerChoice==='rock') {
             result='You lose.';
         } 
-        else if (computerChoice==='paper') {
+        else if (computerChoice==='hand') {
             result='You win.';
         } 
       
     } 
- else if (playerChoice==='paper') {
+ else if (playerChoice==='hand') {
         if (computerChoice==='scissors') {
             result='You lose.';
         } 
@@ -29,54 +29,60 @@ function play(playerChoice){
             result='You win.';
         } 
     }
-}
+ if (result === 'You win.') {
+            score.wins += 1;
+          } else if (result === 'You lose.') {
+            score.losses += 1;
+          } else if (result === 'Draw') {
+            score.draws += 1;
+          }
+    
+    updateScoreElement();
+    localStorage.setItem('score', JSON.stringify(score));
+    updateScoreElement();
+    document.querySelector('.js-result').innerHTML = result;
+    document.querySelector('.js-moves').innerHTML = `You
+  <img class="result" src="images/${playerChoice}.png" class="move-icon">
+  <img class="result" id="id" src="images/${computerChoice}.png" class="move-icon">
+  Computer`;
+    }
+
+
+
 function pickComputerMove() {
     let number = Math.random();
-    let computerMove = '';
+    let computerChoice = '';
     if (number >= 0 && number < 1 / 3) {
         computerChoice = 'rock';
     } 
-    else if (randomNumber >= 1 / 3 && randomNumber < 2 / 3) {
-        computerChoice = 'paper';
+    else if (number >= 1 / 3 && number < 2 / 3) {
+        computerChoice = 'hand';
     } 
-    else if (randomNumber >= 2 / 3 && randomNumber < 1) {
+    else if (number >= 2 / 3 && number < 1) {
         computerChoice = 'scissors';
     }  
     return computerChoice;
-     if (result === 'You win.') {
-      score.wins += 1;
-    } else if (result === 'You lose.') {
-      score.losses += 1;
-    } else if (result === 'Tie.') {
-      score.ties += 1;
-    }
-  
-  }
+     
+}
 
-//
 let score = JSON.parse(localStorage.getItem('score')) || {
     wins: 0,
     losses: 0,
-    ties: 0
+    draws: 0
   };
   
-  updateScoreElement();
 
-   
-    localStorage.setItem('score', JSON.stringify(score));
-  
-    updateScoreElement();
-  
-    document.querySelector('.js-result').innerHTML = result;
-  
-    document.querySelector('.js-moves').innerHTML = `You
-  <img src="images/${playerMove}-emoji.png" class="move-icon">
-  <img src="images/${computerMove}-emoji.png" class="move-icon">
-  Computer`;
-  
-  
+
   function updateScoreElement() {
     document.querySelector('.js-score')
-      .innerHTML = `Wins: ${score.wins}, Losses: ${score.losses}, Ties: ${score.ties}`;
+      .innerHTML = `Wins: ${score.wins}, Losses: ${score.losses}, Draws: ${score.draws}`;
   }
   
+function reset(){
+    score.wins=0;
+    score.losses=0;
+    score.draws=0;
+    localStorage.removeItem('score');
+    document.querySelector('.js-score')
+    .innerHTML=`Wins: ${score.wins},Losses: ${score.losses},Draws: ${score.draws}`;
+}
